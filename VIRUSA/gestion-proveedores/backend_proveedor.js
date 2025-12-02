dc37f05d6d50b40c4031a8a6e13f2226488b5d3d
angelosrb19-ctrl <angelosrb19@gmail.com>
2025-12-01 23:45:19 -0500

    Update backend_proveedor.js

--- VIRUSA/gestion-proveedores/backend_proveedor.js
+++ VIRUSA/gestion-proveedores/backend_proveedor.js
@@ -1,1 +1,11 @@
 
+router.get('/proveedores', (req, res) => {
+  
+  const proveedores = [
+    { id: 1, nombre: 'Proveedor A', empresa: 'Empresa A', direccion: 'Dirección A', estado: 'activo' },
+    { id: 2, nombre: 'Proveedor B', empresa: 'Empresa B', direccion: 'Dirección B', estado: 'inactivo' }
+  ];
+
+  res.json(proveedores);
+});
+
